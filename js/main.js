876c265ae5410325477eea9aefedcad39ca5fdf3
Gavin Hanna <gavhanna@gmail.com>
2016-11-05 21:53:32 +0000

    really rough semi-working prototype

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ jQuery(function ($) {
 
   console.log(boxes["box-1"]);
 
-  function events(){
+  function init(){
     $(".choice div span").on("click", selectIcon);
     $(".box").on("click", placeIcon);
   };
@@ -18,33 +18,71 @@ jQuery(function ($) {
     icon = $(this).text();
     icon === "X" ? iconAI = "O" : iconAI = "X";
     $(".icon-choice-modal").remove();
-    console.log(iconAI);
   };
 
   function placeIcon(){
-    $(this).html("<span>" + icon + "</span>");
-    var fullPlace = $(this).attr("id");
-    removeFromArray(fullPlace, boxes);
-    placeIconAI();
+    if ($(this).text() === "") {
+      $(this).html("<span>" + icon + "</span>");
+      var fullPlace = $(this).attr("id");
+      removeFromArray(fullPlace, boxes);
+      placeIconAI();
+    }
   };
 
   function placeIconAI(){
-    var randomBox = boxes[Math.floor(Math.random() * 9)];
-    if (boxes[randomBox]) {
-      $("#" + boxes[randomBox]).html("<span>" + icon + "</span>");
-    }
-    removeFromArray(randomBox, boxes);
-    console.log(randomBox);
+    var randomNum = Math.floor(Math.random() * 9)
+    var randomBox = boxes[randomNum];
+    if (boxes.length > 0) {
+      if (randomBox !== undefined) {
+        $("#" + randomBox).html("<span>" + iconAI + "</span>");
+        removeFromArray(randomBox, boxes);
+      } else if (randomBox === undefined) {
+        placeIconAI();
+      }
+    } else {console.log("the array is empty");}
+    checkForWinner("X");
+    checkForWinner("O");
   };
 
   function removeFromArray(item, array){
     array.splice( $.inArray(item, array), 1);
   }
 
-  function init(){
-    events();
+  function checkForWinner(winner){
+    if (
+      ($("#box-1").text() === winner &&
+      $("#box-2").text() === winner &&
+      $("#box-3").text() === winner)
+      ||
+      ($("#box-4").text() === winner &&
+      $("#box-5").text() === winner &&
+      $("#box-6").text() === winner)
+      ||
+      ($("#box-7").text() === winner &&
+      $("#box-8").text() === winner &&
+      $("#box-9").text() === winner)
+      ||
+      ($("#box-1").text() === winner &&
+      $("#box-5").text() === winner &&
+      $("#box-9").text() === winner)
+      ||
+      ($("#box-1").text() === winner &&
+      $("#box-4").text() === winner &&
+      $("#box-7").text() === winner)
+      ||
+      ($("#box-2").text() === winner &&
+      $("#box-5").text() === winner &&
+      $("#box-8").text() === winner)
+      ||
+      ($("#box-3").text() === winner &&
+      $("#box-6").text() === winner &&
+      $("#box-9").text() === winner)
+      ) {
+        if (confirm(winner + " wins! Play again?")) {
+          location.reload();
+        }
+    }
   };
-
 
   init();
 });
